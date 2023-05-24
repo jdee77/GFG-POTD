041f22f83b6d5dda42cae7de2bc7c9a4338a59ae
jdee77 <79501910+jdee77@users.noreply.github.com>
2023-05-24 13:51:50 +0530

    Add files via upload

---
+++ 24_05_23_Maximum_Identical_Bowl.cpp
@@ -0,0 +1,80 @@
+// MAXIMUM IDENTICAL BOWL
+/*
+There are N bowls containing cookies. In one operation, you can take one cookie from any of the non-empty bowls and put it into another bowl. If the bowl becomes empty you discard it. You can perform the above operation any number of times. You want to know the maximum number of bowls you can have with an identical number of cookies.
+
+Note: All the non-discarded bowls should contain the identical number of cookies.
+*/
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int getMaximum(int N, vector<int> &arr) {
+        long long sum = 0;
+        long long res = 0;
+        
+        for(int i = 0; i< N; i++) sum+= arr[i];
+        
+        for(int  i = N; i> 0; i--)
+        {
+            if(sum % i == 0)
+            {
+                res = i;
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        
+        
+        vector<int> arr(N);
+        Array::input(arr,N);
+        
+        Solution obj;
+        int res = obj.getMaximum(N, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
